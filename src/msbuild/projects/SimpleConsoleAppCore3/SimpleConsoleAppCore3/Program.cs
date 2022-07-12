5d298f6ae4b5d2e20c1438c16ed8f01a9dccce1e
Dmitrii Bobreshev <v-dbobreshev@microsoft.com>
2022-07-12 09:41:09 +0200

    Test Agent on Win systems

--- src/msbuild/projects/SimpleConsoleAppCore3/SimpleConsoleAppCore3/Program.cs
+++ src/msbuild/projects/SimpleConsoleAppCore3/SimpleConsoleAppCore3/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace SimpleConsoleAppCore3
 {
     class Program
     {
+        const string filePath = @"json.txt";
+
+        
+        public static void Serialize(object obj)
+        {
+            var serializer = new JsonSerializer();        
+            // serializer.MaxDepth = 32;
+
+            using (var sw = new StreamWriter(filePath))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, obj);           
+            }
+        }
+    
+        public static object Deserialize(string path)
+        {
+            var serializer = new JsonSerializer();         
+            // serializer.MaxDepth = 32;
+    
+            using (var sw = new StreamReader(path))
+            using (var reader = new JsonTextReader(sw))
+            {
+                return serializer.Deserialize(reader);
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var deserialized = Deserialize(@"example.json");
+            Console.WriteLine(deserialized);
+            
+            Serialize(deserialized);
         }
     }
 }
