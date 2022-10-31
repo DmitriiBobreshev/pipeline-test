0af243a52e47a01c5df35f5b838280053d996674
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2022-10-31 15:44:43 +0100

    123

--- shell/remove.sh
+++ shell/remove.sh
@@ -4,7 +4,7 @@ PID=`ps -ef | grep node | grep shell_build | awk '{print $2}'`
 CURR_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [[ "" !=  "$PID"  ]]; then
-Kill -9 PID
+kill -9 PID
 fi;
 
 rm -rf $CURR_DIR
