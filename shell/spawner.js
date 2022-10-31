d4f3d838e160314321688dbc77d42ad2fe0f3d4f
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2022-10-31 13:34:08 +0100

    123

--- shell/spawner.js
+++ shell/spawner.js
@@ -1,20 +1,8 @@
 const spawn = require('child_process').spawn;
-const { exec } = require("child_process");
 
-exec('npm install', {cwd: __dirname}, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log( `stderr: ${stderr}`);
-        return;
-    }
-    
-    const child = spawn(process.argv[0], ['shell_build.js'], {
-        detached: true,
-        stdio: ['ignore']
-    });
-      
-    child.unref();
+const child = spawn(process.argv[0], ['shell_build.js'], {
+    detached: true,
+    stdio: ['ignore']
 });
+    
+child.unref();
