65b20904c940375e0a2f93f1480b1ac0dcfe6401
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2023-10-17 12:32:02 +0200

    Devenv test

--- test.cmd
+++ test.cmd
@@ -1,6 +1,6 @@
 set arg1=%1
 set arg2=%2
-CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.com" "%arg1%\LegacyWindowsFormsApp\LegacyWindowsFormsApp.sln" /log "%arg2%\msi_log.xml" /out "%arg2%\out.txt" /Build "Debug" /Project "%arg1%\LegacyWindowsFormsApp/LegacyWindowsFormsApp/LegacyWindowsFormsApp.csproj"
-CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.com" "%arg1%\LegacyWindowsFormsApp\LegacyWindowsFormsApp.sln" /log "%arg2%\msi_log.xml" /out "%arg2%\out.txt" /Build "Debug" /Project "%arg1%\LegacyWindowsFormsApp/BlastFromThePastSetup/BlastFromThePastSetup.vdproj"
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.com" "%arg1%\LegacyWindowsFormsApp\LegacyWindowsFormsApp.sln" /log "%arg2%\msi_log.xml" /out "%arg2%\out.txt" /Build "Debug" /Project "%arg1%\LegacyWindowsFormsApp/LegacyWindowsFormsApp/LegacyWindowsFormsApp.csproj"
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.com" "%arg1%\LegacyWindowsFormsApp\LegacyWindowsFormsApp.sln" /log "%arg2%\msi_log.xml" /out "%arg2%\out.txt" /Build "Debug" /Project "%arg1%\LegacyWindowsFormsApp/BlastFromThePastSetup/BlastFromThePastSetup.vdproj"
 
 exit %errorLevel%
