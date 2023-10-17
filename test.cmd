774a4004b884743190fd9671b9b2d6ebf8d464ba
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2023-10-17 10:41:28 +0200

    Devenv test

--- test.cmd
+++ test.cmd
@@ -1,5 +1,5 @@
 set arg1=%1
 set arg2=%2
-CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.com" "%arg1%\LegacyWindowsFormsApp\LegacyWindowsFormsApp.sln" /log "%arg2%\msi_log.xml" /out "%arg2%\out.txt" /Build "Release" /Project "%arg1%\LegacyWindowsFormsApp/BlastFromThePastSetup/BlastFromThePastSetup.vdproj"
+CALL "D:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe" "%arg1%\LegacyWindowsFormsApp\LegacyWindowsFormsApp.sln" /log "%arg2%\msi_log.xml" /out "%arg2%\out.txt" /Build "Release" /Project "%arg1%\LegacyWindowsFormsApp/BlastFromThePastSetup/BlastFromThePastSetup.vdproj"
 
 exit %errorLevel%
