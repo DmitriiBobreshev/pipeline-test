4ad03b25fcfb39dd61ab62e3958db36f4a831c5d
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2023-11-08 09:45:12 +0100

    MAUI app error .net6

---
+++ src/MAUI/MaiuAzureDevops/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MaiuAzureDevops;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+        // update 3
+    }
+}
---
+++ src/MAUI/MaiuAzureDevops/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MaiuAzureDevops;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		// update 3
+		InitializeComponent();
+	}
+}
