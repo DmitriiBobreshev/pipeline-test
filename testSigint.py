7486a988f29a27bde172ba3fc706d32c277e7923
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2024-08-29 11:37:46 +0200

    Test PythonScript

---
+++ testSigint.py
@@ -0,0 +1,17 @@
+import signal, sys, time
+
+def cancelJob(signum, frame):
+    for i in range(1000):
+        print(i, signal.Signals(signum).name)
+    time.sleep(10)
+    sys.exit(1)
+
+signal.signal(signal.SIGABRT, cancelJob)
+signal.signal(signal.SIGHUP, cancelJob)
+signal.signal(signal.SIGINT, cancelJob)
+signal.signal(signal.SIGQUIT, cancelJob)
+signal.signal(signal.SIGTERM, cancelJob)
+
+for i in range(1000):
+    print(i, "job started")
+time.sleep(600)
